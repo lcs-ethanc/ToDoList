44174b3c82ce5f1a2e731b265dadaaa6b34ddfec
Ethan Chen <echen@lcs.on.ca>
2025-01-28 12:16:38 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Pak Ching Ethan Chen on 2025-01-28.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
